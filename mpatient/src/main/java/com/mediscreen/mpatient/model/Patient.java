a77a724f80900f8cc9e10a88f2c42598d514731b
Jerome Lassus <jerome13250@gmail.com>
2021-09-29 11:58:07 +0200

    JPA auto-ddl to update

--- mpatient/src/main/java/com/mediscreen/mpatient/model/Patient.java
+++ mpatient/src/main/java/com/mediscreen/mpatient/model/Patient.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -24,62 +25,70 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "patient")
+@Table(
+		name = "patient" , 
+		uniqueConstraints = { 
+				@UniqueConstraint(name = "UniqueFirstNameAndLastName", columnNames = { "firstname", "lastname" }) 
+		}
+	)
 public class Patient {
 
 	@ApiModelProperty(
-			  value = "id of patient",
-			  example = "1")
+			value = "id of patient",
+			example = "1")
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	@ApiModelProperty(
-			  value = "First name of patient",
-			  example = "John",
-			  required = true)
-	@Column(name="firstname")
+			value = "First name of patient",
+			example = "John",
+			required = true)
+	@Column(name="firstname", nullable = false, length = 100)
 	@NotBlank
 	@Size(min=1,max=100)
 	String given;
-	
+
 	@ApiModelProperty(
-			  value = "Last name of patient",
-			  example = "Doe",
-			  required = true)
-	@Column(name="lastname")
+			value = "Last name of patient",
+			example = "Doe",
+			required = true)
+	@Column(name="lastname", nullable = false, length = 100)
 	@NotBlank
 	@Size(min=1,max=100)
 	String family;
-	
+
 	@ApiModelProperty(
-			  value = "Patient's date of birth",
-			  example = "2021-09-17",
-			  required = true)
-	@Column(name="birthdate")
+			value = "Patient's date of birth",
+			example = "2021-09-17",
+			required = true)
+	@Column(name="birthdate", nullable = false)
 	@NotNull
 	LocalDate dob;
-	
+
 	@ApiModelProperty(
 			value = "Patient's gender",
 			example = "M",
 			allowableValues= "F, M",
 			required = true)
+	@Column(nullable = false, length = 1)
 	@NotBlank
 	@Pattern(regexp = "[FM]")
 	String sex;
-	
+
 	@ApiModelProperty(
 			value = "Patient's postal address",
 			example = "1st street New-York",
 			required = false)
 	@Size(min=0,max=150)
+	@Column(length = 150)
 	String address;
-	
+
 	@ApiModelProperty(
 			value = "Patient's phone number",
 			example = "111-222-333",
 			required = false)
+	@Column(length = 20)
 	@Size(min=0,max=20)
 	String phone;
 
